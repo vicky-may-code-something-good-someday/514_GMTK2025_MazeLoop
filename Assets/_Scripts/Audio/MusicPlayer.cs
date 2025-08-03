c6ea561ba66ba5a56145f24f6a4863796176365b
Ametrine <kronos2906@gmail.com>
2025-08-03 18:53:27 +0200

    MusicImplemented

--- Assets/_Scripts/Audio/MusicPlayer.cs
+++ Assets/_Scripts/Audio/MusicPlayer.cs
@@ -36,7 +36,7 @@ public class MusicPlayer : MonoBehaviour
         // Set up default source
         defaultSource.clip = defaultMusic;
         defaultSource.loop = true;
-        defaultSource.volume = 1f;
+        defaultSource.volume = 0.5f;
         defaultSource.Play();
 
         // Set up reverse source
@@ -54,11 +54,11 @@ public class MusicPlayer : MonoBehaviour
         if (rewindDate.isRewinding)
         {
             defaultSource.volume = 0f;
-            reverseSource.volume = 1f;
+            reverseSource.volume = 0.5f;
         }
         else
         {
-            defaultSource.volume = 1f;
+            defaultSource.volume = 0.5f;
             reverseSource.volume = 0f;
         }
 
